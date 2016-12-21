0c91654ce578a5e1c415667b31146279b2e47ca5
Aram <aram532@yandex.ru>
2016-12-21 20:17:53 +0400

    uvk

---
+++ OOP1/inheritance/CreditOrganization.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinancialInstitution
+{
+    class CreditOrganization : FinancialInstitution
+    {
+        public CreditOrganization(string name, string adress, long statutoryCapital) : base(name,adress,statutoryCapital)
+        {
+            
+        }
+        public long CreditPortfolio { get; set; }
+    }
+}
