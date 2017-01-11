175c6aea9c7854c68ffe2ccda49a858b4c866dec
Aram <aram532@yandex.ru>
2017-01-11 16:02:57 +0400

    changes 11/1

--- OOP1/ThreeDimensionalForm/Ellipsoid.cs
+++ OOP1/ThreeDimensionalForm/Ellipsoid.cs
@@ -6,22 +6,23 @@ namespace ThreeDimensionalForms
     {
         public Ellipsoid(double radiusA, double radiusB, double radiusC)
         {
-            this.radiusA = radiusA;
-            this.radiusB = radiusB;
-            this.radiusC = radiusC;
+            if(radiusA<=0 || radiusB<=0 || radiusC<=0) throw new Exception("Ellipsoid must have postive radiuses");
+            this.RadiusA = radiusA;
+            this.RadiusB = radiusB;
+            this.RadiusC = radiusC;
         }
 
-        public double radiusA;
-        public double radiusB;
-        public double radiusC;
+        public double RadiusA { get; }
+        public double RadiusB { get; }
+        public double RadiusC { get; }
 
         private const double p = 1.6075; // there is no exacte formule for ellipsoid surface, so it's for approximation
 
-        public override double Volume => 4d / 3 * Math.PI * radiusA * radiusB * radiusC;
+        public override double Volume => 4d / 3 * Math.PI * RadiusA * RadiusB * RadiusC;
 
         public override double Surface
             =>
                 4 * Math.PI *
-                Math.Pow((Math.Pow(radiusA * radiusB, p) + Math.Pow(radiusA * radiusC, p) + Math.Pow(radiusB * radiusC, p)) / 3, p);
+                Math.Pow((Math.Pow(RadiusA * RadiusB, p) + Math.Pow(RadiusA * RadiusC, p) + Math.Pow(RadiusB * RadiusC, p)) / 3, p);
     }
 }
--- OOP1/ThreeDimensionalForm/Parallelepiped.cs
+++ OOP1/ThreeDimensionalForm/Parallelepiped.cs
@@ -6,20 +6,22 @@ namespace ThreeDimensionalForms
     {
         public Parallelepiped(double baseEdgeA, double baseEdgeB, double edgeC, double angleAB, double angleCBase)
         {
-            this.baseEdgeA = baseEdgeA;
-            this.baseEdgeB = baseEdgeB;
-            this.edgeC = edgeC;
-            this.angleAB = angleAB;
-            this.angleCBase = angleCBase;
+            if (baseEdgeA <= 0 || baseEdgeB <= 0 || edgeC <= 0) throw new Exception("Parallelepiped edges must be positive");
+            if (angleAB >= 180 || angleAB <= 0 || angleCBase >= 180 || angleCBase <= 0) throw new Exception("Impossible to create a parallelepiped with specified angles");
+            this.BaseEdgeA = baseEdgeA;
+            this.BaseEdgeB = baseEdgeB;
+            this.EdgeC = edgeC;
+            this.AngleAB = angleAB;
+            this.AngleCBase = angleCBase;
         }
 
-        public double baseEdgeA;
-        public double baseEdgeB;
-        public double edgeC;
-        public double angleAB;
-        public double angleCBase;
+        public double BaseEdgeA { get; }
+        public double BaseEdgeB { get; }
+        public double EdgeC { get; }
+        public double AngleAB { get; }
+        public double AngleCBase { get; }
 
-        public override double Volume => baseEdgeA * baseEdgeB * edgeC * Math.Sin(angleAB) * Math.Sin(angleCBase);
-        public override double Surface => 2 * (baseEdgeA * baseEdgeB + baseEdgeA * edgeC + baseEdgeB * edgeC);
+        public override double Volume => BaseEdgeA * BaseEdgeB * EdgeC * Math.Sin(AngleAB) * Math.Sin(AngleCBase);
+        public override double Surface => 2 * (BaseEdgeA * BaseEdgeB + BaseEdgeA * EdgeC + BaseEdgeB * EdgeC);
     }
 }
